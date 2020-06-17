c4ea027b0b943e598d2485570d87e5ad4e0cc435
Qays <work.worker55@gmail.com>
2020-06-18 00:26:25 +0300

    Structuring the project

---
+++ hanan_qays_shadan/index.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://getbootstrap.com/docs/4.0/components/modal/#events
+
+
+
+
+
+
+
+
+// Objects
+//                                            String Object   number  string
+// Wallet     properties  "id random number"  Name   Currency Balance Description
+// set balance(updateBalance)=> this.balance = updateBalance(balance);
+//             ^this is a method form transaction 
+
+// Transaction  properties  amount  Date() date note tags  
+
+// Expense    extends Transaction   -  
+// method updateBalance function(balance) => newBalance 
+
+
+// Income      extends Transaction   +
+// method updateBalance function(balance) => newBalance 
+
+
+// Currency    properties id name symbol 
