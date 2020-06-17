54c94748e6f7b45759108000d80cc5e2b2174045
Qays <work.worker55@gmail.com>
2020-06-18 00:56:53 +0300

    Adding bootstrap and linking style.css and javascript file

--- hanan_qays_shadan/index.js
+++ hanan_qays_shadan/index.js
@@ -22,18 +22,48 @@
 
 // Objects
 //                                            String Object   number  string
-// Wallet     properties  "id random number"  Name   Currency Balance Description
+// Wallet     properties  "id random number"  Name   Currency Balance Description listOfTransactions
 // set balance(updateBalance)=> this.balance = updateBalance(balance);
-//             ^this is a method form transaction 
+//             ^this is a method form transaction
+// addNewTransaction(transaction) 
+// read from localStorage()=>Wallets List 
+/////////////// read all transactionsForWalletFromStorage()=> listOfTransaction
+// renderTransactions(ul from Dom)=> change the dom 
 
 // Transaction  properties  amount  Date() date note tags  
+// renderTransaction()=> html  except the price text
+
 
 // Expense    extends Transaction   -  
 // method updateBalance function(balance) => newBalance 
+// super.renderTransaction() renderTransaction() only the price text
 
 
 // Income      extends Transaction   +
 // method updateBalance function(balance) => newBalance 
+// super.renderTransaction() renderTransaction() only the price text
+
 
 
 // Currency    properties id name symbol 
+
+
+
+
+
+class Transaction {
+    constructor(name) {
+        this.name
+    }
+}
+class Expense extends Transaction {
+
+}
+
+class Income extends Transaction {
+
+}
+
+console.log(
+
+    (new Income('nnnn') instanceof Expense));
